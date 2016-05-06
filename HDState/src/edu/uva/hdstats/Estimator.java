e5cace660729d26314bf86ae8faefb99308b148c
bertrandx <hx6d@virginia.edu>
2016-05-06 18:46:47 -0400

    rtyrty

--- HDState/src/edu/uva/hdstats/Estimator.java
+++ HDState/src/edu/uva/hdstats/Estimator.java
@@ -1,12 +1,26 @@
 package edu.uva.hdstats;
 
+import Jama.Matrix;
+import edu.uva.hdstats.da.PseudoInverse;
+
 public abstract class Estimator {
 	public static double lambda = 0.00001;
 	public static int iter = 5;
-	public static double stop=0.0005;
-	
+	public static double stop = 0.0005;
+
 	public abstract double[][] covariance(double[][] samples);
+
 	public abstract void covarianceApprox(double[][] samples);
+
 	public abstract double[] getMean(double[][] samples);
+
+	public static double[][] inverse(double[][] _covar) {
+		try {
+			return new Matrix(_covar).inverse().getArray();
+		} catch (Exception e) {
+			return PseudoInverse.inverse(new Matrix(_covar)).getArray();
+		}
+
+	}
 
 }
