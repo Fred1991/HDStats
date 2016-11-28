e5e5c5f64f96eef6dbbdb63d8b975abf8b127e75
Haoyi Xiong <xhyccc@gmail.com>
2016-11-27 18:10:53 -0600

    ensemble

---
+++ HDState/src/edu/uva/hdstats/da/AdaBoostTreeClassifier.java
@@ -0,0 +1,24 @@
+package edu.uva.hdstats.da;
+
+import smile.classification.*;
+public class AdaBoostTreeClassifier implements Classifier<double[]>{
+	AdaBoost classifier= null;
+	
+	public AdaBoostTreeClassifier(double[][] d, int[] g, int nTree) {
+		this.classifier= new AdaBoost(d, g, nTree);
+	}
+
+
+	@Override
+	public int predict(double[] x) {
+		// TODO Auto-generated method stub
+		return this.classifier.predict(x);
+	}
+
+	@Override
+	public int predict(double[] x, double[] posteriori) {
+		// TODO Auto-generated method stub
+		return this.classifier.predict(x,posteriori);
+	}
+
+}
--- HDState/src/edu/uva/hdstats/da/BayesLDA.java
+++ HDState/src/edu/uva/hdstats/da/BayesLDA.java
@@ -68,7 +68,7 @@ public class BayesLDA extends OLDA {
 		int fDOF = Math.min(Math.max(this.precision.length, g.length) * 10, 2000);
 		Random r = new Random(System.currentTimeMillis());
 		InverseWishartDistribution iwd=new InverseWishartDistribution(mf.copyArray(super.pooledInverseCovariance),
-																		Math.max(this.pooledInverseCovariance.length+1, g.length-1));
+																		Math.max(this.pooledInverseCovariance.length+1, g.length-1));  
 		while (pooledClassifiers.size() < size) {
 			Matrix mtx = sample(r, meanV, covarianceSqrt, fDOF);
 			// NonSparseEstimator nse=new NonSparseEstimator();
